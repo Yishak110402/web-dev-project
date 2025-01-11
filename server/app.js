23a9241f400f7241c022b341dfa24ed3046d708a
yishak <yishak.110402@gmail.com>
2025-01-11 22:42:10 +0300

    first commit

---
+++ server/app.js
@@ -0,0 +1,33 @@
+const express = require("express")
+const mongoose = require("mongoose")
+const cors = require("cors")
+const authRouter = require("./routes/authRoutes")
+
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+app.use("/auth", authRouter)
+
+mongoose.connect("mongodb+srv://yishak:rfTtsGRqkPr5ILhL@write-wave.3yjawuk.mongodb.net/writewave?retryWrites=true&w=majority&appName=write-wave",{
+    useCreateIndex:true,
+    useNewUrlParser:true,
+    useFindAndModify:false,
+    useUnifiedTopology:true
+}).then(()=>{
+    console.log("DB Connection Successful");    
+}).catch((err)=>{
+    console.log(err.message);
+    
+})
+
+
+app.all((req, res)=>{
+    return res.json({
+        status:"success",
+        message:"Test successfull"
+    })
+})
+app.listen(6969, ()=>{
+    console.log("Listening on port 6969")
+})
